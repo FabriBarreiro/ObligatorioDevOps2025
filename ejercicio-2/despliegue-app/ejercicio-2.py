c2a28b86e67a06a2c74ee0fc28c7e5b1fa6e2b88
Fabri Barreiro <fabribarreiro30@gmail.com>
2025-11-21 20:45:36 -0300

    Se agrega busqueda de AMI AL2023

--- ejercicio-2/despliegue-app/ejercicio-2.py
+++ ejercicio-2/despliegue-app/ejercicio-2.py
@@ -1,4 +1,18 @@
 import boto3
 
+def obtenerAMI():
+    ec2 = boto3.client("ec2", region_name="us-east-1")
+    resp = ec2.describe_images(
+        Owners=["amazon"],
+        Filters=[
+            {"Name": "name", "Values": ["al2023-ami-*"]},
+            {"Name": "architecture", "Values": ["x86_64"]},
+        ]
+    )["Images"]
+
+    resp.sort(key=lambda x: x["CreationDate"], reverse=True)
+    return resp[0]["ImageId"]
+
 ec2=boto3.client('ec2')
 rds=boto3.client('rds')
+ssm=boto3.client('ssm')
